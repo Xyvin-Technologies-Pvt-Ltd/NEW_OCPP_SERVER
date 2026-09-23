7412aa81232d2e241ddd4e3a26057578b49580ee
sreeramis8888 <sreeramis8888@gmail.com>
2026-09-23 09:25:08 +0545

    fix: update service URLs to use secure HTTPS for production environment

--- src/services/user-service-api.js
+++ src/services/user-service-api.js
@@ -13,7 +13,7 @@ const setURL = async () => {
 
     try {
         if (process.env.NODE_ENV === 'production') {
-            USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'http://13.203.2.34:5050';
+            USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'https://cms.goecm.com.np';
         } else {
             const userUrlSecret = await getSecret();
             USER_SERVICE_URL = userUrlSecret.USER_SERVICE_URL;
